d4ab8ccd085e93740fe6a7f9c85e057816066410
renganathan-code <renganathan.pakkiyaraj@gmail.com>
2025-12-13 22:40:23 +0530

    Add JS

--- script.js
+++ script.js
@@ -25,3 +25,43 @@ closeBtn.addEventListener("click",function(){
     sideBar.style.transition = "1s"
     offerBar.style.display = "flex"
 })
+
+//Selecting Slider images and Tags
+
+const sliderImage = document.querySelector(".slide--image")
+const sliderRightBtn = document.querySelector(".slide--rightbtn")
+const sliderLeftBtn = document.querySelector(".slide--leftbtn")
+const sliderMargin = 0
+
+sliderRightBtn.addEventListener("click",function(){
+    
+    if(sliderMargin == 0){
+        sliderImage.style.marginLeft = "-35%"
+        sliderImage.style.transition = "1s"
+    }
+
+    else if(sliderMargin == 200){
+        sliderImage.style.marginLeft = "-35%"
+        sliderImage.style.transition = "1s"
+    }
+
+    else{
+        sliderImage.style.marginright = "35%"
+        sliderImage.style.transition = "1s"
+    }
+
+})
+
+sliderLeftBtn.addEventListener("click",function(){
+    
+    if(sliderMargin == 200){
+        sliderImage.style.marginRight = "-35%"
+        sliderImage.style.transition = "1s"
+    }
+
+    else{
+        sliderImage.style.marginLeft = "-35%"
+        sliderImage.style.transition = "1s"
+    }
+
+})
